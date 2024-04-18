4b339bd145ad79a3f2f583ecc91aba8848a4ee4a
liguozi <liguozi777@gmail.com>
2024-04-18 13:39:47 +0800

    Render the first scene and geometric object,use OrbitControls to view the object

--- 01-three_basic/src/main/main01.js
+++ 01-three_basic/src/main/main01.js
@@ -1,10 +1,7 @@
 import * as THREE from "three";
-// 导入轨道控制器
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-
 // console.log(THREE);
 
-// 目标：使用控制器查看3d物体
+// 目标：了解three.js最基本的内容
 // 1.创建场景
 const scene = new THREE.Scene();
 // 2.创建相机
@@ -33,14 +30,4 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 // 将webgl渲染的canvas内容添加到body
 document.body.appendChild(renderer.domElement);
 // 使用渲染器，通过相机将场景渲染进来
-// renderer.render(scene, camera);
-
-// 创建轨道控制器
-const controls = new OrbitControls(camera, renderer.domElement);
-
-function render(){
-    renderer.render(scene,camera);
-    // 渲染下一帧的时候就会调用render函数
-    requestAnimationFrame(render);
-}
-render()
+renderer.render(scene, camera);
