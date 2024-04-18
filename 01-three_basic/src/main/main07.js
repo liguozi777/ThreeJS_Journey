1b928eecaf378d1fcf69d6af425d66bae73791e6
liguozi <liguozi777@gmail.com>
2024-04-18 15:34:33 +0800

    Add GSAP animation for cube position and double click event listener

--- 01-three_basic/src/main/main07.js
+++ 01-three_basic/src/main/main07.js
@@ -55,34 +55,8 @@ scene.add(axesHelper);
 // 设置时钟
 const clock = new THREE.Clock();
 // 设置动画
-var animate1 = gsap.to(cube.position, {
-  x: 5,
-  duration: 5,
-  ease: "power1.inOut",
-  // 设置重复的次数，无限次循环-1
-  repeat: -1,
-  // 往返运动,
-  yoyo: true,
-  // 延迟2秒运动
-  delay: 2,
-  onComplete: () => {
-    console.log("动画完成");
-  },
-  onStart: () => {
-    console.log("动画开始");
-  },
-});
+gsap.to(cube.position, { x: 5, duration: 5, ease: "power1.inOut" });
 gsap.to(cube.rotation, { x: Math.PI, duration: 5, ease: "power1.inOut" });
-window.addEventListener("dblclick", () => {
-  // console.log(animate1)
-  if (animate1.isActive()) {
-    // 暂停
-    animate1.pause();
-  } else {
-    // 恢复
-    animate1.resume();
-  }
-});
 function render() {
   renderer.render(scene, camera);
   // 渲染下一帧的时候就会调用render函数
