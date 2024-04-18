30458060d92d6486e5164b540dbb9aa246689d81
liguozi <liguozi777@gmail.com>
2024-04-18 16:02:51 +0800

    Set damping for controls and implement viewport responsiveness on window resize

--- 01-three_basic/src/main/main08.js
+++ 01-three_basic/src/main/main08.js
@@ -48,8 +48,6 @@ document.body.appendChild(renderer.domElement);
 
 // 创建轨道控制器
 const controls = new OrbitControls(camera, renderer.domElement);
-// 设置控制器阻尼，让控制器更有真实效果,必须在动画循环里调用.update()
-controls.enableDamping = true;
 
 // 添加坐标轴辅助器
 const axesHelper = new THREE.AxesHelper(5);
@@ -86,22 +84,8 @@ window.addEventListener("dblclick", () => {
   }
 });
 function render() {
-  controls.update();
   renderer.render(scene, camera);
   // 渲染下一帧的时候就会调用render函数
   requestAnimationFrame(render);
 }
 render();
-
-// 监听画面变化，更新渲染画面
-window.addEventListener("resize", () => {
-  // console.log("画面变化了")
-  // 更新摄像头
-  camera.aspect = window.innerWidth / window.innerHeight;
-  // 更新摄像机的投影矩阵
-  camera.updateProjectionMatrix();
-  // 更新渲染器
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  // 设置渲染器的像素比
-  renderer.setPixelRatio(window.devicePixelRatio);
-});
